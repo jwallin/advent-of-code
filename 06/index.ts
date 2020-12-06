fc0a5386899f4d2fd3009b7382ae625184553528
Johan Wallin <j.e.wallin@gmail.com>
2020-12-06 13:47:28 +0100

    Day 6: Part 2

--- 06/index.ts
+++ 06/index.ts
@@ -1,11 +1,21 @@
-import { lines, toChars } from '../utils';
+import { lines, toChars, unique } from '../utils';
 
 async function partOne() {
-  const groups: string[] = (await lines('input.txt')).join(' ').split('  ');;
+  const groups: string[] = (await lines('input.txt')).join(' ').split('  ');
   const totalCount = groups.map(x => x.split(' ').map(y => toChars(y)).flat())
-                            .map(z => new Set(z).size)
-                            .reduce((a, b) => a + b, 0);
+                           .map(z => new Set(z).size)
+                           .reduce((a, b) => a + b, 0);
   console.log(totalCount)
 }
 
-partOne()
+async function partTwo() {
+  const groups: string[][] = (await lines('input.txt')).join(' ').split('  ').map(x => x.split(' '));
+  const total = groups.reduce((prev, curr) => {
+    const answers:string[] = unique(curr.map(y => toChars(y)).flat());
+    const gm = curr.map(x => x.split(''));
+    return prev + answers.filter(q => gm.every(x => x.find(y => y === q))).length;
+  }, 0);
+  console.log(total);
+}
+
+partTwo()
--- utils/index.ts
+++ utils/index.ts
@@ -9,3 +9,4 @@ export const countDigits = (input: number): number => toChars(String(input)).len
 export const isValidYear = (input: number, min: number, max: number): boolean => countDigits(input) === 4 && input >= min && input <= max;
 export const hasAllValuesFrom = (input:any[], target:any[]): boolean => target.every((v:any) => input.includes(v));
 export const range = (max:number):number[] => [...Array(max).keys()];
+export const unique = <U>(input: U[]): U[] => Array.from(new Set(input));
