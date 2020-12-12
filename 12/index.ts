35f4c7172ee77db899c1ed2c28494a707289b779
Johan Wallin <j.e.wallin@gmail.com>
2020-12-12 11:29:39 +0100

    Make code generic for part 1 & 2

--- 12/index.ts
+++ 12/index.ts
@@ -62,44 +62,46 @@ function rotatePosition(pos: Position, dir:number, angle: number): Position {
   }
 }
 
+function applyInstructions(instructions:Instruction[], shipPos: Position, wayPoint:Position, moveWaypoint: boolean): Position {
+  instructions.forEach(i => {
+    if (i.direction === DIRECTION_FORWARD) {
+      // Move ship forward to direction / waypoint
+      shipPos = moveInDirection(shipPos, wayPoint, i.value);
+    } else if (i.direction in DIRECTIONS) {
+      // Move in direction withouth updating it
+      const dir = DIRECTIONS[i.direction];
+      if (moveWaypoint) {
+        // Move waypoint in direction by x steps
+        wayPoint = moveInDirection(wayPoint, dir, i.value);
+      } else {
+        // Move ship in direction by x steps
+        shipPos = moveInDirection(shipPos, dir, i.value);
+      }
+    } else {
+      // Rotate the waypoint / direction
+      wayPoint = rotatePosition(wayPoint, LeftRight[i.direction], i.value);
+    }
+  });
+  return shipPos;
+}
+
 async function partOne() {
   const instructions = (await getInput()).map(x => toInstruction(x));
   let currentPos: Position = { x: 0, y: 0 };
   let direction: Position = DIRECTIONS['E'];
   
-  instructions.forEach(i => {
-    if (i.direction === DIRECTION_FORWARD) {
-      // Move forward in saved direction
-      currentPos = moveInDirection(currentPos, direction, i.value);
-    } else if (i.direction in DIRECTIONS) {
-      // Move in direction withouth updating it
-      const d = DIRECTIONS[i.direction];
-      currentPos = moveInDirection(currentPos, d, i.value);
-    } else {
-      // Left & Right should turn ship and calculate new direction
-      direction = rotatePosition(direction, LeftRight[i.direction], i.value);
-    }
-  });
-  console.log(Math.abs(currentPos.x) + Math.abs(currentPos.y));
+  const shipPos = applyInstructions(instructions, currentPos, direction, false);
+  console.log(Math.abs(shipPos.x) + Math.abs(shipPos.y));
 }
+
 
 async function partTwo() {
   const instructions = (await getInput()).map(x => toInstruction(x));
   let currentPos: Position = { x: 0, y: 0 };
   let wayPoint: Position = { x: 10, y: -1 };  // relative to ship
   
-  instructions.forEach(i => {
-    if (i.direction === DIRECTION_FORWARD) {
-      currentPos = moveInDirection(currentPos, wayPoint, i.value);
-    } else if (i.direction in DIRECTIONS) {
-      // Move in direction withouth updating it
-      const dir = DIRECTIONS[i.direction];
-      wayPoint = moveInDirection(wayPoint, dir, i.value);
-    } else {
-      wayPoint = rotatePosition(wayPoint, LeftRight[i.direction], i.value);
-    }
-  });
-  console.log(Math.abs(currentPos.x) + Math.abs(currentPos.y));
+  const shipPos = applyInstructions(instructions, currentPos, wayPoint, true);
+  console.log(Math.abs(shipPos.x) + Math.abs(shipPos.y));
 }
 
 partOne()
