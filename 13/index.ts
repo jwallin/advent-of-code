4f40075a4651169c59d04cfb8592e8752dbb529c
Johan Wallin <j.e.wallin@gmail.com>
2020-12-13 13:17:00 +0100

    Day 13

---
+++ 13/index.ts
@@ -0,0 +1,43 @@
+import { lines } from '../utils';
+
+async function getInput():Promise<string[]> {
+  return await lines('input.txt');
+}
+
+async function partOne() {
+  const timetable = await getInput();
+  const timestamp = Number(timetable[0]);
+  const ids = timetable[1].split(',').filter(x => x !== 'x').map(Number);
+
+  for (let i = 0; i < 100; i++) {
+    const currentTime = timestamp + i;
+    const departure = ids.find(id => currentTime % id  === 0);
+    if (departure !== undefined) {
+      console.log(departure * i);
+      break;
+    }
+  }
+}
+
+async function partTwo() {
+  const timetable = await getInput();
+  const ids = timetable[1].split(',').map(x => Number(x));
+
+  let time = 0;
+  let multiplier = ids[0];
+  for (let i = 1; i < ids.length; i++) {
+    if (isNaN(ids[i])) {
+      continue;
+    }
+    while (true) {
+      time += multiplier;
+      if ((time + i) % ids[i] === 0) {
+        multiplier *= ids[i];
+        break;
+      }
+    }
+  }
+  console.log(time);
+}
+
+partTwo();
