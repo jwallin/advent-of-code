a102f7ed56807fd68398c2bea21c13f79c92d394
Johan Wallin <j.e.wallin@gmail.com>
2020-12-15 19:30:43 +0100

    Day 15: Optimize for part 2

--- 15/index.ts
+++ 15/index.ts
@@ -1,29 +1,22 @@
-import { getInput, toChars, sum } from '../utils';
+import { getInput } from '../utils';
 
-
-function speakNum(i:number, numbers:number[], startingNumbers:number[]):number {
-  if (i < startingNumbers.length) {
-    return startingNumbers[i];
-  } else {
-    const lastNumSpoken = numbers[i - 1];
-    const lastTimeSpoken = numbers.slice(0, i - 1).lastIndexOf(lastNumSpoken);
-    if (lastTimeSpoken === -1) {
-      return 0;
+async function playGame(rounds:number) {
+  const startingNumbers:number[] = (await getInput())[0].split(',').map(Number);
+  
+  let map = new Map<number, number>();
+  let n:number = startingNumbers[startingNumbers.length - 1];
+  let last:number | undefined;
+  
+  for (let i = 0; i < rounds; i++) {
+    last = map.get(n) || 0;
+    map.set(n, i);
+    if (i < startingNumbers.length) {
+      n = startingNumbers[i];
     } else {
-      return i - 1 - lastTimeSpoken;
+      n = last ? i - last : 0; 
     }
   }
+  console.log(n);
 }
 
-async function partOne() {
-  const startingNumbers:number[] = (await getInput())[0].split(',').map(Number);
-  const MAX = 2020;
-  const spokenNumbers:number[] = [];
-  for (let i = 0; i < MAX; i++) {
-    const n = speakNum(i, spokenNumbers, startingNumbers);
-    spokenNumbers.push(n);
-    console.log(`${i + 1}: ${n}`);
-  }
-}
-
-partOne();
+playGame(30000000);
