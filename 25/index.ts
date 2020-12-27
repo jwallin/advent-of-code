30507202d175253a6bb2fcef7e0a2f6ea142cb59
Johan Wallin <j.e.wallin@gmail.com>
2020-12-27 16:49:41 +0100

    Day 25

---
+++ 25/index.ts
@@ -0,0 +1,33 @@
+const DIVIDER = 20201227;
+const MAX = 100000000;
+
+function transform(subject: number, loopSize: number, toFind: number | undefined = undefined) {
+  let v = 1;
+  for (let i = 0; i < loopSize; i++) {
+    v = (v * subject) % DIVIDER;
+    if (v === toFind) {
+      return i + 1;
+    }
+  }
+  
+  if (toFind) {
+    throw new Error(`Couldnt find number ${toFind}`);
+  }
+
+  return v;
+}
+
+
+function partOne() {
+  let cardPbk = 2084668;
+  let doorPbk = 3704642;
+  const subjectNumber = 7;
+
+  const cardLoopSize = transform(subjectNumber, MAX, cardPbk);
+  const doorLoopSize = transform(subjectNumber, MAX, doorPbk);
+
+  console.log(transform(cardPbk, doorLoopSize));
+  console.log(transform(doorPbk, cardLoopSize));
+}
+
+partOne();
