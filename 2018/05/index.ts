06c76a304a94f41ae34fe6d4a865ed6e5afb5fb7
Johan Wallin <j.e.wallin@gmail.com>
2021-10-04 16:34:32 +0200

    2018 Day 5, simplify things

--- 2018/05/index.ts
+++ 2018/05/index.ts
@@ -1,34 +1,15 @@
-import { getInput, toChars, pairs } from '../../utils';
-
-function adjacentPairs<T>(array: T[]): T[][] {
-  const p:T[][] = [];
-  for (let i = 1; i < array.length; i++) {
-    p.push([array[i - 1], array[i]])
-  }
-  return p;
-}
+import { getInput, toChars } from '../../utils';
 
 function isOppositePolarity(a:string, b:string) {
   return a.toLowerCase() === b.toLowerCase() && a !== b;
 }
 
 function react(chars:string[]) {
-  let currentLength = chars.length;
-  let start = 0;
-  while (true) {
-    for (let i = start; i < chars.length - 1; i++) {
-      if (isOppositePolarity(chars[i], chars[i+1])) {
-        //Remove char, exit
-        chars.splice(i, 2);
-        start = Math.max(i - 3, 0);
-        break;
-      }
+  for (let i = 0; i < chars.length - 1; i++) {
+    if (isOppositePolarity(chars[i], chars[i+1])) {
+      chars.splice(i, 2);
+      i = Math.max(i - 2, 0);
     }
-    
-    if (currentLength === chars.length) {
-      break;
-    }
-    currentLength = chars.length;
   }
   return chars;
 }
