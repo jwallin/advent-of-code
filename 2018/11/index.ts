b7747317fd9946422e5551ad862a50c50be1083f
Johan Wallin <j.e.wallin@gmail.com>
2021-10-08 16:48:12 +0200

    Day 11 2018

--- 2018/11/index.ts
+++ 2018/11/index.ts
@@ -1,5 +1,7 @@
-import { Matrix } from '../../utils/matrix';
-import { Position, sum } from '../../utils/position';
+import { sum } from '../../utils';
+import { Position, sum as sumPosition } from '../../utils/position';
+
+const SIZE = 300;
 
 function powerLevel(p:Position, serial:number) {
   const rackID = p.x + 10;
@@ -10,24 +12,82 @@ function powerLevel(p:Position, serial:number) {
   return Number(vals[vals.length - 3] || 0) - 5;
 }
 
-function getTotalPower(p:Position, serial:number) {
+function getTotalPower(p:Position, serial:number, size:number) {
   let s = 0;
-  for (let x = 0; x < 3; x++) {
-    for (let y = 0; y < 3 ; y++) {
-      s += powerLevel(sum(p, {x, y}), serial);
+  for (let x = 0; x < size; x++) {
+    for (let y = 0; y < size ; y++) {
+      s += powerLevel(sumPosition(p, {x, y}), serial);
     }
   }
   return s;
 }
 
-async function partOne(serial: number) {
-  const m = new Matrix<number>();
-  let maxPower = 0;
-  let maxCoord:Position = {x:-1, y: -1};
+function fixedKadane(a: number[], k: number) {
+  let maxSum = Number.MIN_VALUE;
+  let maxIndex = -1;
+  for (let i = 0; i < a.length - k; i++) {
+    const s = a.slice(i, i + k).reduce(sum);
+    if (s > maxSum) {
+      maxSum = s;
+      maxIndex = i;
+    }
+  }
+  return [maxSum, maxIndex];
+}
+
+function getMatrix(serial: number, size: number) {
+  const m:number[][] = [];
+  for (let y = 0; y < size; y++) {
+    m[y] = [];
+    for (let x = 0; x < size; x++) {
+      m[y][x] = powerLevel(sumPosition({x, y}, {x:1, y:1}), serial);
+    }
+  }
+  return m;
+}
+
+function kadane2D(a:number[][]) {
+  const preSum:number[][] = a.reduce<number[][]>((acc,y) => [...acc, y.slice()], []);
+  for (let i = 1; i < preSum.length; i++) {
+    for (let j = 0; j < preSum.length; j++) { 
+      preSum[i][j] += preSum[i - 1][j];
+    }
+  }
+
+  let maxSum = 0;
+  let maxPos:Position = {x:-1, y: -1};
+  let maxSize = 0;
+  for (let yStart = 0; yStart < preSum.length; yStart++) {
+    for (let yEnd = yStart; yEnd < preSum.length; yEnd++) {
+      let sums:number[] = [];
+      for (let i = 0; i < preSum.length; i++) {
+        if (yStart > 0) {
+          sums[i] = preSum[yEnd][i] - preSum[yStart - 1][i];
+        } else {
+          sums[i] = preSum[yEnd][i];
+        }
+      }
+
+      const size = yEnd-yStart + 1;
+      const [s, xStart] = fixedKadane(sums, size);
+      if (s > maxSum) {
+        maxSum = s;
+        maxPos = {x: xStart + 1, y: yStart + 1}
+        maxSize = size; 
+      }
+    }
+  }
+  return [maxPos.x, maxPos.y, maxSize];
+}
+
+function partOne(serial: number) {
+  let maxPower = Number.MIN_VALUE;
+  let maxCoord:Position = {x: -1, y: -1};
+  const SQUARE_SIZE = 3;
   
-  for (let x = 1; x <= 300 - 3; x++) {
-    for (let y = 1; y <= 300 - 3; y++) {
-      const total = getTotalPower({x, y}, serial);
+  for (let x = 1; x <= SIZE - SQUARE_SIZE; x++) {
+    for (let y = 1; y <= SIZE - SQUARE_SIZE; y++) {
+      const total = getTotalPower({x, y}, serial, SQUARE_SIZE);
       
       if (total > maxPower) {
         maxPower = total;
@@ -38,9 +98,10 @@ async function partOne(serial: number) {
   console.log(maxPower, maxCoord);
 }
 
-async function partTwo() {
-  
+function partTwo(serial: number) {
+  const m = getMatrix(serial, SIZE);
+  console.log(kadane2D(m).join(','))
 }
-  
 
-partOne(8199);
+
+partTwo(8199);
