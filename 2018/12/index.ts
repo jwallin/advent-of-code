fd9dbc3863e9707c2408baaa424b7e276728d823
Johan Wallin <j.e.wallin@gmail.com>
2021-10-08 19:26:18 +0200

    2018 Day 12 part 2

--- 2018/12/index.ts
+++ 2018/12/index.ts
@@ -1,49 +1,76 @@
 import { getInput } from '../../utils';
 import { KeyVal } from '../../utils/types';
 
-
-async function partOne() {
-  const input = (await getInput());
-  let state = (input.shift() || '').split(': ')[1].split('')
-  input.shift()
-  
-  let rules:KeyVal<string> = {};
+function getRules(input: string[]) {
+  const rules: KeyVal<string> = {};
   input.forEach(i => {
     const [key, val] = i.split(' => ');
     rules[key] = val;
   });
+  return rules;
+}
 
-  let shiftCount = 0;
-  console.log(0, state.join(''))
-  for (let g = 1; g <= 20; g++) {
-    let newState: string[] = [];
-    const f = ['.', '.', '.', ...state, '.', '.', '.']
-    for (let i = 0; i < f.length; i++) {
-     
-      const pots = f.slice(i-2, i + 3);
-      const newPlant = rules[pots.join('')] || '.';
-      newState[i] = newPlant;
-  
-    }
-    console.log(g, newState.join('').substring(shiftCount))
-    state = newState;
-    shiftCount += 3;
-
-  }
-
-  const total = state.reduce((acc, curr, i) => {
+function sumOfPlantIndexes(state: string[], shiftCount: number) {
+  return state.reduce((acc, curr, i) => {
     if (curr === '#') {
       return acc + i - shiftCount;
     }
     return acc;
   }, 0);
+}
+
+async function partOne() {
+  const input = (await getInput());
+  let state = (input.shift() || '').split(': ')[1].split('');
+  input.shift();
+  
+  const rules: KeyVal<string> = getRules(input);
+
+  let shiftCount = 0;
+  for (let g = 1; g <= 20; g++) {
+    state = generation(state, rules);
+    shiftCount += 3;
+  }
+
+  const total = sumOfPlantIndexes(state, shiftCount);
   
   console.log(total);
 }
 
-async function partTwo() {
-  const input = (await getInput()).map(Number);
+function generation(state: string[], rules: KeyVal<string>) {
+  const newState: string[] = [];
+  const f = ['.', '.', '.', ...state, '.', '.', '.'];
+  for (let i = 0; i < f.length; i++) {
+    const pots = f.slice(i - 2, i + 3);
+    newState[i] = rules[pots.join('')] || '.';;
+  }
+  return newState;
 }
-  
 
-partOne();
+async function partTwo() {
+  const input = (await getInput());
+  let state = (input.shift() || '').split(': ')[1].split('');
+  input.shift()
+  
+  const rules: KeyVal<string> = getRules(input);
+
+  let shiftCount = 0;
+  let prevTotal = 0;
+  for (let g = 1; g <= 200; g++) {
+    state = generation(state, rules);
+    shiftCount += 3;
+    const total = sumOfPlantIndexes(state, shiftCount);
+    
+    console.log(g, total, total - prevTotal);
+    prevTotal = total;
+  }
+
+  const total = sumOfPlantIndexes(state, shiftCount);
+  
+  console.log(total);
+
+  console.log((50000000000 - 200) * 59 + total);
+}
+
+partTwo();
+
