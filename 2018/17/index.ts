3cfbc3bc18eadba8ebd619e1c584e0216c2b0ccd
Johan Wallin <j.e.wallin@gmail.com>
2021-10-26 18:00:45 +0200

    2018 Day 17 part 1

--- 2018/17/index.ts
+++ 2018/17/index.ts
@@ -20,69 +20,67 @@ function parseInput(s:string):Position[] {
   });
 }
 
+
+function isBucket(m: Matrix<string>, p: Position) {
+  const { left, right } = getBucket(m, p);
+  if (left === -1 || right === -1) {
+    return false;
+  }
+  const bucket = m.getRow(p.y).slice(left + 1, right);
+  return !bucket.includes('.');
+}
+
+
+function getBucket(m: Matrix<string>, p: Position) {
+  const row = m.getRow(p.y);
+  const left = row.lastIndexOf('#', p.x - 1);
+  const right = row.indexOf('#', p.x + 1);
+  return { left, right };
+}
+
+function flow(m:Matrix<string>, p:Position) {
+  const belowPos = sum(p, {x:0, y:1});
+
+  if (m.get(belowPos) === '.') {
+    m.set(belowPos, '|');
+    flow(m, belowPos);
+  }
+
+  if (m.get(belowPos) === '#' || m.get(belowPos) === '~') {
+    const leftRight = [sum(p, {x: -1, y:0}), sum(p, {x: 1, y:0})];
+    leftRight.forEach(x => {
+      if (m.get(x) === '.') {
+        m.set(x, '|');
+        flow(m, x);
+      }
+    })
+  }
+
+  if (isBucket(m, p)) {
+    const { left, right } = getBucket(m, p);
+    arrayBetween(left + 1, right - 1).forEach(x => m.set({x, y: p.y}, '~'));  
+  }
+}
+
 async function partOne() {
   const input = (await getInput()).map(parseInput).flat();
   const m = new Matrix<string>();
-  const spring = {x:500, y: 0};
+  const spring = { x:500, y: 0 };
+
+  const minY = min(input).y;
+  const maxY = max(input).y;
   
-  m.fill(min(input.concat(spring)), max(input), '.');
-  m.set(spring, '+');
+  m.fill(sum(min(input.concat(spring)), {x: -1, y: 0}), sum(max(input), {x: 1, y: 0}), '.');
   input.forEach(i => m.set(i, '#'));
   
-  let prev = '';
-  const flows = [spring];
+  flow(m, spring);
 
-  for (let i = 0; i < 5000; i++) {
-    //const flows = m.asArray().filter(x => m.get(x)?.match(/\||\+/));
-    flows.forEach(p => {
-      const below = m.get(sum(p, {x:0, y:1}));
-      if (below === '|') {
-        return;
-      } 
-      if(below === '.') {
-        m.set(sum(p, {x:0, y:1}), '|');
-        flows.push(sum(p, {x:0, y:1}))
-      } else if (below === '#' || below === '~') {
-        //Try to left and right
-        const leftPos = sum(p, {x:-1, y:0});
-        const rightPos = sum(p, {x:1, y:0});
-        if (m.get(leftPos) === '.') {
-          m.set(leftPos, '|');
-          flows.push(leftPos)
-        }
-        if (m.get(rightPos) === '.') {
-          m.set(rightPos, '|');
-          flows.push(rightPos)
-        }
-        // If row is bucket
-        const row = m.getRow(p.y);
-        const left = row.lastIndexOf('#', p.x - 1);
-        const right = row.indexOf('#', p.x + 1);
-        const belowRow = m.getRow(p.y + 1).slice(left + 1, right -1);
-
-        if (left !== -1 && right !== -1 && belowRow.every(x => x !== '.')) {
-          arrayBetween(left + 1, right - 1).forEach(x => m.set({x, y: p.y}, '~'));  
-        }
-        
-      }
-    })
-    
-    const curr = m.toString();
-    if (curr === prev) {
-      break;
-    }
-    
-    prev = curr;
-  }
-
-  console.log(prev);
-  console.log('');
-  console.log(m.values().filter(x => x === '|' || x === '~').length)
+  const v = m.rows.slice(minY, maxY + 1).reduce((acc, x) => acc + x.filter(x => x === '|' || x === '~').length, 0);
+  console.log(v)
 }
 
 async function partTwo() {
   const input = (await getInput()).map(Number);
 }
-  
 
 partOne();
