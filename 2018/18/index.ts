74f5f3db9f700f81b268ffe09c520f067c0e3378
Johan Wallin <j.e.wallin@gmail.com>
2021-10-26 19:34:43 +0200

    2018 Day 18 part 2

--- 2018/18/index.ts
+++ 2018/18/index.ts
@@ -15,6 +15,8 @@ function getNextValue(m:Matrix<string>, p:Position) {
       return (adjacentValues.filter(x => x === LUMBERYARD).length >= 3) ? LUMBERYARD : TREES;
     case LUMBERYARD: 
       return (adjacentValues.filter(x => x === LUMBERYARD).length >= 1 && adjacentValues.filter(x => x === TREES).length >= 1) ? LUMBERYARD : OPEN;
+    default:
+      throw new Error('Unexpected value');
   }
 }
 
@@ -22,8 +24,8 @@ async function partOne() {
   const input = (await getInput()).map(x => x.split(''));
   let m = new Matrix(input);
   
-  for (let i = 0; i < 10; i++) {
-    const b = m.clone();
+  for (let i = 1; i <= 10; i++) {
+    const b = new Matrix<string>();
     m.asArray().forEach(p => {
       b.set(p, getNextValue(m, p));
     });
@@ -38,8 +40,33 @@ async function partOne() {
 }
 
 async function partTwo() {
-  const input = (await getInput()).map(Number);
+  const input = (await getInput()).map(x => x.split(''));
+  let m = new Matrix(input);
+  
+  const window:number[] = [];
+  const START_INDEX = 1000;
+  const WINDOW_LENGTH = 28;
+  for (let i = 1; i <= START_INDEX + WINDOW_LENGTH; i++) {
+    const b = new Matrix<string>();
+    m.asArray().forEach(p => {
+      b.set(p, getNextValue(m, p));
+    });
+    m = b;
+
+    // Create a window array
+    if ( i >= START_INDEX )  {
+      const wooded = m.values().filter(x => x === TREES).length;
+      const lumberyards = m.values().filter(x => x === LUMBERYARD).length;
+      const total = wooded * lumberyards;
+      window[i - START_INDEX] = total;
+    }
+  }
+
+  console.log(window);
+  const n = 1000000000;
+  console.log(n, window[(n - START_INDEX) % WINDOW_LENGTH]);
+ 
 }
   
 
-partOne();
+partTwo();
