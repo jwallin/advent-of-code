1508bdda736628d1d2da420bc54e3c3aac24095b
Johan Wallin <j.e.wallin@gmail.com>
2021-12-20 00:30:39 +0100

    Extract LinkedList to separate module

--- 2018/09/index.ts
+++ 2018/09/index.ts
@@ -1,78 +1,5 @@
 import { getInput } from '../../utils';
-
-class LinkedList {
-  private _value: number;
-  private _left: LinkedList;
-  private _right: LinkedList;
-
-  constructor(value: number, left: LinkedList | undefined = undefined, right: LinkedList | undefined = undefined) {
-    this._value = value;
-    this._left = left || this ;
-    this._right = right || this;
-    
-    this._left.right = this;
-    this._right.left = this;
-  }
-
-  get left(): LinkedList {
-    return this._left;
-  }
-
-  set left(val: LinkedList) {
-    this._left = val;
-  }
-
-  get right(): LinkedList {
-    return this._right;
-  }
-
-  set right(val: LinkedList) {
-    this._right = val;
-  }
-
-  get value(): number {
-    return this._value;
-  }
-
-  stepLeft(steps: number): LinkedList {
-    if (steps === 0) {
-      return this;
-    }
-    return this._left.stepLeft(steps - 1);
-  }
-
-  stepRight(steps: number): LinkedList {
-    if (steps === 0) {
-      return this;
-    }
-    return this._right.stepRight(steps - 1);
-  }
-
-  remove(): LinkedList {
-    if (this.left) {
-      this.left.right = this.right;
-    }
-    if (this.right) {
-      this.right.left = this.left;
-    }
-    const r = this.right;
-    return r;
-  }
-
-  toString(): string {
-    const v = [this.value];
-    let curr: LinkedList = this;
-    while (true) {
-      curr = curr.right;
-      if (curr === this) {
-        break;
-      }
-      v.push(curr.value)
-    }
-    return v.join(' ')
-  }
-  
-}
+import { LinkedList } from '../../utils/linkedList';
 
 function parseInput(input: string) {
   const regex = /^(\d+) players; last marble is worth (\d+) points/;
@@ -86,7 +13,7 @@ function parseInput(input: string) {
 
 function play(numPlayers: number, maxMarble: number): number {
   const players = new Array(numPlayers).fill(0);
-  let currentMarble: LinkedList = new LinkedList(0);
+  let currentMarble = new LinkedList(0);
   currentMarble.left = currentMarble;
   currentMarble.right = currentMarble;
   for (let i = 1; i <= maxMarble; i++) {
@@ -94,9 +21,9 @@ function play(numPlayers: number, maxMarble: number): number {
     if (i % 23 === 0) {
       const toRemove = currentMarble.stepLeft(7);
       players[currPlayer] += i + toRemove.value;
-      currentMarble = toRemove.remove();
+      currentMarble = toRemove.remove() as LinkedList<number>;
     } else {
-      const toLeft = currentMarble.right;
+      const toLeft = currentMarble.right as LinkedList<number>;
       const toRight = toLeft.right;
       currentMarble = new LinkedList(i, toLeft, toRight);
     }
---
+++ utils/linkedList.ts
@@ -0,0 +1,99 @@
+import { KeyVal } from "./types";
+
+export class LinkedList<T> {
+    private _value: T;
+    private _left: LinkedList<T> | undefined;
+    private _right: LinkedList<T> | undefined;
+  
+    constructor(value: T, left: LinkedList<T> | undefined = undefined, right: LinkedList<T> | undefined = undefined) {
+      this._value = value;
+      this._left = left;
+      this._right = right;
+      
+      if (this._left) {
+          this._left.right = this;
+        }
+      if (this._right) {
+          this._right.left = this;
+      }
+    }
+  
+    get left(): LinkedList<T> | undefined{
+      return this._left;
+    }
+  
+    set left(val: LinkedList<T> | undefined) {
+      this._left = val;
+    }
+  
+    get right(): LinkedList<T> | undefined {
+      return this._right;
+    }
+  
+    set right(val: LinkedList<T> | undefined) {
+      this._right = val;
+    }
+  
+    get value(): T {
+      return this._value;
+    }
+  
+    stepLeft(steps: number): LinkedList<T> {
+      if (steps === 0) {
+        return this;
+      }
+      if (!this._left) {
+          throw new Error('Couldnt step left');
+      }
+      return this._left.stepLeft(steps - 1);
+    }
+  
+    stepRight(steps: number): LinkedList<T> {
+      if (steps === 0) {
+        return this;
+      }
+      if (!this._right) {
+        throw new Error('Couldnt step left');
+    }
+      return this._right.stepRight(steps - 1);
+    }
+
+    insertRight(v: T): LinkedList<T> {
+        this.right = new LinkedList(v, this, this.right);
+        return this.right;
+    }
+  
+    remove(): LinkedList<T> | undefined {
+      if (this.left) {
+        this.left.right = this.right;
+      }
+      if (this.right) {
+        this.right.left = this.left;
+      }
+      const r = this.right;
+      return r;
+    }
+  
+    toString(): string {
+      const v = [this.value];
+      let curr: LinkedList<T> | undefined = this;
+      while (true) {
+        curr = curr.right;
+        if (curr === this || curr === undefined) {
+          break;
+        }
+        v.push(curr.value)
+      }
+      return v.join(' ')
+    }
+
+    static fromArray<U>(arr: U[]):LinkedList<U> {
+        const els = arr.slice();
+        const first = new LinkedList(els.shift() as U);
+        let curr = first;
+        while (els.length > 0) {
+            curr = new LinkedList(els.shift() as U, curr);
+        }
+        return first;
+    }
+  }
