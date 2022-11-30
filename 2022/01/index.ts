dcd543006542f9c6ae1db3855bd74a9501ed0300
Johan Wallin <j.e.wallin@gmail.com>
2022-11-30 23:56:21 +0100

    Get ready for 2022

---
+++ 2022/01/index.ts
@@ -0,0 +1,12 @@
+import { getInput } from '../../utils';
+
+async function partOne() {
+  const input = (await getInput()).map(Number);
+}
+
+async function partTwo() {
+  const input = (await getInput()).map(Number);
+}
+  
+
+partOne();
