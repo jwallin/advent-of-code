43b3ff70c846da43bab48fbb9ee552ce89896713
Johan Wallin <j.e.wallin@gmail.com>
2022-12-01 09:22:52 +0100

    Day 1 2022

--- 2022/01/index.ts
+++ 2022/01/index.ts
@@ -1,12 +1,17 @@
-import { getInput } from '../../utils';
+import { getInput, sortDescending, splitArrayBy, sum } from '../../utils';
 
 async function partOne() {
-  const input = (await getInput()).map(Number);
+  const input = (await getInput());
+  const vals = splitArrayBy(input, '').map(x => x.map(y => Number(y)).reduce(sum));
+  console.log(Math.max(...vals));
 }
 
 async function partTwo() {
-  const input = (await getInput()).map(Number);
+  const input = (await getInput());
+  const vals = splitArrayBy(input, '').map(x => x.map(y => Number(y)).reduce(sum));
+  vals.sort(sortDescending);
+  console.log(vals.slice(0, 3).reduce(sum));
 }
   
-
 partOne();
+partTwo();
--- utils/index.ts
+++ utils/index.ts
@@ -92,3 +92,12 @@ export function flipObject(o: KeyVal<any>): KeyVal<string> {
     return ret;
   }, {});
 }
+
+export const sortDescending = (a: number, b: number): 1 | -1 | 0 => {
+  if (a < b) {
+    return 1;
+  } else if (a > b) {
+    return -1;
+  }
+  return 0;
+};
