774fa4dd228fdd5ed495001932c4ef876b160f82
Johan Wallin <wallin@spotify.com>
2024-12-28 01:53:22 +0100

    Day 3 part 2

--- 2024/03/index.ts
+++ 2024/03/index.ts
@@ -2,7 +2,7 @@ import { getInput, multiply } from '../../utils';
 
 async function partOne() {
   const input = (await getInput()).join();
-  const re = /mul\(?(\d+),?(\d+)\)/g;
+  const re = /mul\(?(\d{1,3}),?(\d{1,3})\)/g;
   let v: RegExpExecArray | null = null;
   let sum = 0;
   do {
@@ -16,8 +16,28 @@ async function partOne() {
 }
 
 async function partTwo() {
-  const input = (await getInput()).map(Number);
+  const input = (await getInput()).join();
+  const re = /do\(\)|don't\(\)|mul\(?(\d{1,3}),?(\d{1,3})\)/g;
+  let v: RegExpExecArray | null = null;
+  let sum = 0;
+  let enabled = true;
+  do {
+    v = re.exec(input)
+    if (v) {
+      const cmd = v[0];
+      if (cmd === 'do()') {
+        enabled = true;
+      } else if (cmd === 'don\'t()') {
+        enabled = false;
+      } else {
+        if (enabled) {
+          sum += v.slice(1, 3).map(Number).reduce(multiply);
+        }
+      }
+    }
+  } while (v);
+  console.log(sum)
 }
-  
 
 partOne();
+partTwo();
